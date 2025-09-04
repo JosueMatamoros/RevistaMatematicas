55b047c7acc87d92d6d9a301969ddbc49d03ed24
JosueMatamoros <1002matamoros@gmail.com>
2025-09-04 14:46:26 -0600

    feat: prod paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,10 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/Articulos/RevistaDigital_V26_n1_2025_Alfaro',
-        destination: '/Articulos/V26/N1_2025/alfaro',
-      },
-      {
-        source: '/Articulos/RevistaDigital_V26_n1_2025_Parra',
-        destination: '/Articulos/V26/N1_2025/Parra',
-      },
-      {
-        source: '/Articulos/RevistaDigital_V26_n1_2025_Oviedo',
-        destination: '/Articulos/V26/N1_2025/Oviedo',
-      },
-      {
-        source: '/Articulos/RevistaDigital_V26_n1_2025_Mosquera',
-        destination: '/Articulos/V26/N1_2025/mosquera',
-      },
-    ]
-  },
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: isProd ? '/servicios/revistamatematica/pruebas' : '',
+  assetPrefix: isProd ? '/servicios/revistamatematica/pruebas/' : '',
 };
 
 export default nextConfig;
