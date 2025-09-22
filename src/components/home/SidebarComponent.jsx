4eb5052052b9a71021197751bcd1916b307ed5b6
JosueMatamoros <1002matamoros@gmail.com>
2025-09-22 15:27:20 -0600

    feat: solve production issues and class errors

--- src/components/home/SidebarComponent.jsx
+++ src/components/home/SidebarComponent.jsx
@@ -16,7 +16,7 @@ export default function SidebarComponent({ isOpen, setIsOpen }) {
             <div className="w-full max-w-sm min-w-[200px] px-4 pt-4 space-x-2">
               <div className="relative flex items-center">
                 <input
-                  class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-black/40 rounded-md pl-3 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                  className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-black/40 rounded-md pl-3 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
                   placeholder="Buscar Artículos ..."
                 />
 
@@ -31,9 +31,9 @@ export default function SidebarComponent({ isOpen, setIsOpen }) {
                     className="w-4 h-4"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     />
                   </svg>
                 </button>
