4a5578baed217cfe0cda3a9642d3d6a8565c2406
JosueMatamoros <1002matamoros@gmail.com>
2025-10-06 20:20:35 -0600

    feat: SearchBar component created in common directory

---
+++ src/components/common/SearchBar.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { IoSearchSharp } from "react-icons/io5";
+
+export default function SearchBar() {
+  return (
+    <div className="relative w-full ">
+      <input
+        type="text"
+        placeholder="Buscar..."
+        className="w-full rounded-full border border-gray-300 py-2 pl-4 pr-10 text-sm text-slate-700 placeholder:text-slate-400 focus:outline-none shadow-sm"
+      />
+      <button
+        type="button"
+        className="absolute right-1 top-1/2 -translate-y-1/2 bg-tec-blue-primary hover:bg-tec-blue-primary/90 text-white p-2 rounded-full transition-all duration-200"
+      >
+        <IoSearchSharp size={16} />
+      </button>
+    </div>
+  );
+}
