3e5ba22a96c8aba6ce320a0a9f2b20fb1a97957f
JosueMatamoros <1002matamoros@gmail.com>
2025-12-07 20:39:33 -0600

    feat: data structure update and new public content for V25 N2 2025

--- src/data/issues/index.js
+++ src/data/issues/index.js
@@ -1,41 +1,32 @@
 // src/data/issues/index.js
-import N1_2025 from "./N1_2025.json";
-import N2_2024 from "./N2_2024.json";
-import N1_2024 from "./N1_2024.json";
-import N2_2023 from "./N2_2023.json";
+import V26_N1_2025 from "./V26_N1_2025.json";
+import V25_N2_2025 from "./V25_N2_2025.json";
+import V25_N1_2024 from "./V25_N1_2024.json";
+
 
 export const issues = [
   {
-    id: "N1_2025",
+    id: "V26_N1_2025",
     volume: 26,
     number: 1,
     date: "Agosto 2025 - Febrero 2026",
     label: "Actual",
-    data: N1_2025,
+    data: V26_N1_2025,
   },
   {
-    id: "N2_2024",
+    id: "V25_N2_2025",
     volume: 25,
     number: 2,
     date: "Marzo 2025 - Agosto 2025",
     label: "Reciente",
-    data: N2_2024,
+    data: V25_N2_2025,
   },
   {
-    id: "N1_2024",
-    volume: 24,
+    id: "V25_N1_2024",
+    volume: 25,
     number: 1,
-    date: "Marzo 2025 - Agosto 2025",
+    date: "Agosto 2024 - Febrero 2025",
     label: "",
-    data: N1_2024,
-  },
-  {
-    id: "N2_2023",
-    volume: 23,
-    number: 2,
-    date: "Marzo 2024 - Agosto 2024",
-    label: "",
-    data: N2_2023,
-  },
-
+    data: V25_N1_2024,
+  }
 ];
