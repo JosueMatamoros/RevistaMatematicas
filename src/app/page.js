e2c9529012e746269cf65c845d25d5d57c0e625d
JosueMatamoros <1002matamoros@gmail.com>
2026-02-09 13:17:13 -0600

    feat: add books to main page and issue pages

--- src/app/page.js
+++ src/app/page.js
@@ -35,6 +35,7 @@ export default function Home() {
           <ArticlesList
             title={issueData.issueTitle}
             articles={issueData.articles}
+            books={issueData.books || []}
           />
         </main>
 
