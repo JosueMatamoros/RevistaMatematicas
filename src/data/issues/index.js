ab6f42f09784ee491d117d03563fb2531c0e330a
JosueMatamoros <1002matamoros@gmail.com>
2026-03-23 10:33:53 -0600

    feat: Add new articles for V24 N1 2023 and V24 N2 2024, and update index.js

--- src/data/issues/index.js
+++ src/data/issues/index.js
@@ -3,6 +3,8 @@ import V26_N1_2025 from "./V26_N1_2025.json";
 import V25_N2_2025 from "./V25_N2_2025.json";
 import V25_N1_2024 from "./V25_N1_2024.json";
 import V26_N2_2026 from "./V26_N2_2026.json";
+import V24_N2_2024 from "./V24_N2_2024.json";
+import V24_N1_2023 from "./V24_N1_2023.json";
 
 
 export const issues = [
@@ -33,5 +35,20 @@ export const issues = [
     number: 1,
     date: "Agosto 2024 - Febrero 2025",
     data: V25_N1_2024,
-  }
+  },
+  {
+    id: "V24_N2_2024",
+    volume: 24,
+    number: 2,
+    date: "Marzo 2024 - Agosto 2024",
+    data: V24_N2_2024,
+  },
+  {
+    id: "V24_N1_2023",
+    volume: 24,
+    number: 1,
+    date: "Agosto 2023 - Febrero 2024",
+    data: V24_N1_2023,
+  },
+
 ];
